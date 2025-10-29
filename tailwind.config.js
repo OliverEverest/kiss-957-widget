28e165f5b1342e868efecfc5df96741ad5db1be3
Oliver Everest <rougecharlestcanlom@proton.me>
2025-10-29 11:25:36 +0800

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app/**/*.{js,jsx,ts,tsx}",
+    "./pages/**/*.{js,jsx,ts,tsx}",
+    "./components/**/*.{js,jsx,ts,tsx}"
+  ],
+  theme: {
+    extend: {
+      animation: {
+        "spin-slow": "spin 6s linear infinite",
+      },
+    },
+  },
+  plugins: [],
+};
